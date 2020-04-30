ed314fc546cef65a7c467eaca0c722e349a334e5
Grant Goodman <grantfgoodman@gmail.com>
2020-04-30 01:52:07 -0500

    Added functionality to studio

--- app/src/main/java/com/example/animalrecognition/ActivityStudio.java
+++ app/src/main/java/com/example/animalrecognition/ActivityStudio.java
@@ -1,19 +1,45 @@
 package com.example.animalrecognition;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
 
 public class ActivityStudio extends AppCompatActivity {
 
     private static int RESULT_LOAD_IMAGE = 1;
     static FirebaseAuth auth;
+    private GridView gridView;
     private Button buttonHome, buttonGo, buttonCamera, buttonUpload;
+    int takeImageCode = 10001;
+
+    private void handleUpload(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayStream);
+
+        String uID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        final StorageReference reference = FirebaseStorage.getInstance().getReference()
+                .child(uID + "_ImagesList")
+                .child(uID + ".jpeg");
+    }
 
     private void startHome() {
         Intent intHome = new Intent(ActivityStudio.this, ActivityHome.class);
@@ -30,7 +56,10 @@ public class ActivityStudio extends AppCompatActivity {
     }
 
     private void takePicture() {
-
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, takeImageCode);
+        }
     }
 
     private void getPictureFromGallery() {
@@ -51,6 +80,18 @@ public class ActivityStudio extends AppCompatActivity {
         buttonGo = findViewById(R.id.buttonGo);
         buttonCamera = findViewById(R.id.buttonCamera);
         buttonUpload = findViewById(R.id.buttonUpload);
+        gridView = findViewById(R.id.gridView);
+
+        gridView.setAdapter(new com.example.animalrecognition.ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), FullScreenActivity.class);
+                intent.putExtra("id", position);
+                startActivity(intent);
+            }
+         });
 
         buttonHome.setOnClickListener(new View.OnClickListener() {
             @Override
