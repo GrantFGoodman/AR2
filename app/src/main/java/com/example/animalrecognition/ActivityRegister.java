0dd2412fbd3e2265850c485753afb9087f220600
Grant Goodman <grantfgoodman@gmail.com>
2020-05-01 01:56:11 -0500

    fixed crash with hitting "go" without uploading image first

--- app/src/main/java/com/example/animalrecognition/ActivityRegister.java
+++ app/src/main/java/com/example/animalrecognition/ActivityRegister.java
@@ -1,4 +1,5 @@
 package com.example.animalrecognition;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
--- app/src/main/java/com/example/animalrecognition/ActivityStudio.java
+++ app/src/main/java/com/example/animalrecognition/ActivityStudio.java
@@ -86,12 +86,14 @@ public class ActivityStudio extends AppCompatActivity {
         finish();
     }
 
+    /*
     private void startClassify() {
         Intent intClassify = new Intent(ActivityStudio.this, ActivityClassify.class);
         startActivity(intClassify);
 
         finish();
     }
+     */
 
     private void takePicture() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -157,7 +159,6 @@ public class ActivityStudio extends AppCompatActivity {
         buttonUpload = findViewById(R.id.buttonUpload);
         picture = findViewById(R.id.picture);
 
-
         /*
         gridView.setAdapter(new com.example.animalrecognition.ImageAdapter(this));
 
@@ -191,89 +192,93 @@ public class ActivityStudio extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                FirebaseCustomLocalModel model = new FirebaseCustomLocalModel.Builder()
-                        .setAssetFilePath("recognitionModel.tflite")
-                        .build();
+                if (currentImage != null) {
+                    FirebaseCustomLocalModel model = new FirebaseCustomLocalModel.Builder()
+                            .setAssetFilePath("recognitionModel.tflite")
+                            .build();
 
-                FirebaseModelInterpreterOptions options = new FirebaseModelInterpreterOptions.Builder(model).build();
-                FirebaseModelInterpreter interpreter = null;
+                    FirebaseModelInterpreterOptions options = new FirebaseModelInterpreterOptions.Builder(model).build();
+                    FirebaseModelInterpreter interpreter = null;
 
-                {
-                    try {
-                        interpreter = FirebaseModelInterpreter.getInstance(options);
-                    } catch (FirebaseMLException e) {
-                        Log.i("MLKit", e.getMessage());
+                    {
+                        try {
+                            interpreter = FirebaseModelInterpreter.getInstance(options);
+                        } catch (FirebaseMLException e) {
+                            Log.i("MLKit", e.getMessage());
+                        }
                     }
-                }
 
-                FirebaseModelInputOutputOptions inputOutputOptions = null;
+                    FirebaseModelInputOutputOptions inputOutputOptions = null;
 
-                {
+                    {
+                        try {
+                            inputOutputOptions = new FirebaseModelInputOutputOptions.Builder()
+                                    .setInputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, size, size, depth})
+                                    .setOutputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, output})
+                                    .build();
+                        } catch (FirebaseMLException e) {
+                            Log.i("MLKit", e.getMessage());
+                        }
+                    }
+
+                    Bitmap bitmap = Bitmap.createScaledBitmap(currentImage, size, size, true);
+
+                    int batchNum = 0;
+                    float[][][][] input = new float[1][size][size][depth];
+                    for (int x = 0; x < size; x++) {
+                        for (int y = 0; y < size; y++) {
+                            int pixel = bitmap.getPixel(x, y);
+                            // Normalize channel values to [-1.0, 1.0]. This requirement varies by
+                            // model. For example, some models might require values to be normalized
+                            // to the range [0.0, 1.0] instead.
+                            input[batchNum][x][y][0] = (Color.red(pixel) - 127) / 128.0f;
+                            input[batchNum][x][y][1] = (Color.green(pixel) - 127) / 128.0f;
+                            input[batchNum][x][y][2] = (Color.blue(pixel) - 127) / 128.0f;
+                        }
+                    }
+
+                    FirebaseModelInputs inputs = null;
                     try {
-                        inputOutputOptions = new FirebaseModelInputOutputOptions.Builder()
-                                .setInputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, size, size, depth})
-                                .setOutputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, output})
+                        inputs = new FirebaseModelInputs.Builder()
+                                .add(input)  // add() as many input arrays as your model requires
                                 .build();
                     } catch (FirebaseMLException e) {
                         Log.i("MLKit", e.getMessage());
                     }
-                }
 
-                Bitmap bitmap = Bitmap.createScaledBitmap(currentImage, size, size, true);
+                    interpreter.run(inputs, inputOutputOptions)
+                            .addOnSuccessListener(
+                                    new OnSuccessListener<FirebaseModelOutputs>() {
+                                        @Override
+                                        public void onSuccess(FirebaseModelOutputs result) {
+                                            float[][] output = result.getOutput(0);
+                                            float[] probabilities = output[0];
 
-                int batchNum = 0;
-                float[][][][] input = new float[1][size][size][depth];
-                for (int x = 0; x < size; x++) {
-                    for (int y = 0; y < size; y++) {
-                        int pixel = bitmap.getPixel(x, y);
-                        // Normalize channel values to [-1.0, 1.0]. This requirement varies by
-                        // model. For example, some models might require values to be normalized
-                        // to the range [0.0, 1.0] instead.
-                        input[batchNum][x][y][0] = (Color.red(pixel) - 127) / 128.0f;
-                        input[batchNum][x][y][1] = (Color.green(pixel) - 127) / 128.0f;
-                        input[batchNum][x][y][2] = (Color.blue(pixel) - 127) / 128.0f;
-                    }
-                }
+                                            float highestProbability = 0;
+                                            String likeliest = "";
 
-                FirebaseModelInputs inputs = null;
-                try {
-                    inputs = new FirebaseModelInputs.Builder()
-                            .add(input)  // add() as many input arrays as your model requires
-                            .build();
-                } catch (FirebaseMLException e) {
-                    Log.i("MLKit", e.getMessage());
-                }
+                                            for (int i = 0; i < probabilities.length; i++) {
+                                                Log.i("MLKit", String.format("%s: %1.4f", labels[i % labels.length], probabilities[i]));
 
-                interpreter.run(inputs, inputOutputOptions)
-                        .addOnSuccessListener(
-                                new OnSuccessListener<FirebaseModelOutputs>() {
-                                    @Override
-                                    public void onSuccess(FirebaseModelOutputs result) {
-                                        float[][] output = result.getOutput(0);
-                                        float[] probabilities = output[0];
-
-                                        float highestProbability = 0;
-                                        String likeliest = "";
-
-                                        for (int i = 0; i < probabilities.length; i++) {
-                                            Log.i("MLKit", String.format("%s: %1.4f", labels[i % labels.length], probabilities[i]));
-
-                                            if (probabilities[i] > highestProbability) {
-                                                likeliest = labels[i % labels.length];
-                                                highestProbability = probabilities[i];
+                                                if (probabilities[i] > highestProbability) {
+                                                    likeliest = labels[i % labels.length];
+                                                    highestProbability = probabilities[i];
+                                                }
                                             }
-                                        }
 
-                                        Toast.makeText(ActivityStudio.this, "Image contains a(n) " + likeliest, Toast.LENGTH_LONG).show();
-                                    }
-                                })
-                        .addOnFailureListener(
-                                new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        Log.i("MLKit", e.getMessage());
-                                    }
-                                });
+                                            Toast.makeText(ActivityStudio.this, "Image contains a(n) " + likeliest, Toast.LENGTH_LONG).show();
+                                        }
+                                    })
+                            .addOnFailureListener(
+                                    new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.i("MLKit", e.getMessage());
+                                        }
+                                    });
+                } else {
+                    Toast.makeText(ActivityStudio.this, "Please upload or capture photo", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
